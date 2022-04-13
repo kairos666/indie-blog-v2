16d337063cbf756f716b1711364f60acccea9c4f
David Maggi <musashi.miyamoto81@gmail.com>
2022-04-13 09:16:28 +0200

    AppFrame component (modal animation)

--- packages/blog/components/ui-elements/AppFrame.tsx
+++ packages/blog/components/ui-elements/AppFrame.tsx
@@ -1,7 +1,27 @@
 import { FC, ReactNode, Children } from 'react';
 import { useBreakpointMatcher } from '../../hooks/useBreakpointMatcher';
+import { useTransition, animated } from '@react-spring/web';
 import coreStyle from '../../styles/AppFrame.core.module.scss';
 import themeStyle from '../../styles/AppFrame.theme.module.scss';
+
+// right side drawer animation
+const desktopModalTransitionProperties = {
+    keys: null,
+    from: { opacity: 0, transform: 'translate3d(100%,0,0) scale(1, 1)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0) scale(1, 1)' },
+    leave: { opacity: 0, transform: 'translate3d(100%,0,0) scale(1, 1)' },
+    exitBeforeEnter: true
+};
+
+// mobile modal animation (zoom in and out of view)
+const mobileModalTransitionProperties = {
+    keys: null,
+    from: { opacity: 0, transform: 'scale(0.75, 0.75) translate3d(0%,0,0)' },
+    enter: { opacity: 1, transform: 'scale(1, 1) translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'scale(0.75, 0.75) translate3d(0%,0,0)' },
+    exitBeforeEnter: true
+};
+// !!! to smoothly switch between both types of transition they must have complete transform overlap (scale & translate3d)
 
 type AppFrameProps = {
     desktopBreakpoint: number,
@@ -13,11 +33,17 @@ const AppFrame:FC<AppFrameProps> = ({ desktopBreakpoint, modal, actionsChildren,
     const isDesktop = useBreakpointMatcher(windowWidth => desktopBreakpoint <= windowWidth);
     const hasActionMenu = Children.count(actionsChildren) !== 0;
 
+    const modalTransition = useTransition(modal, isDesktop ? desktopModalTransitionProperties : mobileModalTransitionProperties);
+
     return (
         <main className={ `${ coreStyle['apf-AppFrame'] } ${ hasActionMenu ? coreStyle['apf-AppFrame--has-action-menu'] : coreStyle['apf-AppFrame--no-action-menu'] } ${ isDesktop ? coreStyle['apf-AppFrame--desktop'] : coreStyle['apf-AppFrame--mobile'] }` }>
             <div className={ coreStyle['apf-AppFrame_Content'] }>{ children }</div>
             { hasActionMenu ? <menu className={ `${ coreStyle['apf-AppFrame_ActionMenu'] } ${ themeStyle['apf-AppFrame_ActionMenu'] } ${ isDesktop ? themeStyle['apf-AppFrame_ActionMenu--desktop'] : themeStyle['apf-AppFrame_ActionMenu--mobile'] }` }>{ actionsChildren }</menu> : null }
-            { (modal) ? <aside className={ `${ coreStyle['apf-AppFrame_Modal'] } ${ isDesktop ? coreStyle['apf-AppFrame_Modal--desktop'] : coreStyle['apf-AppFrame_Modal--mobile'] } ${ themeStyle['apf-AppFrame_Modal'] } ${ isDesktop ? themeStyle['apf-AppFrame_Modal--desktop'] : themeStyle['apf-AppFrame_Modal--mobile'] }` }>{ modal }</aside> : null }
+            {modalTransition((style, modal) => 
+                <animated.aside style={ style } className={ `${ coreStyle['apf-AppFrame_Modal'] } ${ isDesktop ? coreStyle['apf-AppFrame_Modal--desktop'] : coreStyle['apf-AppFrame_Modal--mobile'] } ${ themeStyle['apf-AppFrame_Modal'] } ${ isDesktop ? themeStyle['apf-AppFrame_Modal--desktop'] : themeStyle['apf-AppFrame_Modal--mobile'] }` }>
+                    { modal }
+                </animated.aside>
+            )}
         </main>
     )
 }
