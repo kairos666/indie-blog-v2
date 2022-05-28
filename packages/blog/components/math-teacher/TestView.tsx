3009345acd7d47a736c6d5b53812d98e1ea7955a
David Maggi <musashi.miyamoto81@gmail.com>
2022-05-28 10:20:37 +0200

    QuestionConfigurator

--- packages/blog/components/math-teacher/TestView.tsx
+++ packages/blog/components/math-teacher/TestView.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode, useCallback, useEffect } from 'react';
 import { useMathTeacherState } from '../../state-managers-hooks/match-teacher/useMathTeacherState';
+import QuestionnaireConfigurator from './QuestionnaireConfigurator';
 import QuestionnaireHeader from './QuestionnaireHeader';
 import styles from './TestView.module.scss';
 
@@ -8,13 +9,12 @@ type TestViewProps = {
 };
 
 const TestView:FC<TestViewProps> = ({ displayDetailHandler }) => {
-    const { availableTables, testState, testConfig, canChangeTestConfig } = useMathTeacherState(state => ({
-        availableTables:state.test.availableTables,
+    const { testState, testConfig, canChangeTestConfig } = useMathTeacherState(state => ({
         testState: state.test.testState,
         testConfig: state.test.testConfig,
         canChangeTestConfig: (state.test.testState === "PRE_TEST")
     }));
-    const { toggleSelectedTable, startTest, endTest, resetTest, changeTestConfig } = useMathTeacherState(state => ({
+    const { startTest, endTest, resetTest } = useMathTeacherState(state => ({
         startTest: state.test.startTest,
         endTest: state.test.endTest,
         resetTest: state.test.resetTest,
@@ -22,27 +22,13 @@ const TestView:FC<TestViewProps> = ({ displayDetailHandler }) => {
         changeTestConfig: state.test.changeTestConfig
     }));
     const testQuestionary = useMathTeacherState(state => state.test.questionnaire);
-    const { questions, results, currentQuestionIndex } = testQuestionary;
 
     const startTestHandler = useCallback(() => { displayDetailHandler(null); startTest(); }, [startTest, displayDetailHandler]);
     const endTestHandler = useCallback(() => { displayDetailHandler(null); endTest(); }, [endTest, displayDetailHandler]);
     const resetTestHandler = useCallback(() => { displayDetailHandler(null); resetTest(); }, [resetTest, displayDetailHandler]);
     const triggerTestConfigSetupHandler = useCallback(() => {
-        displayDetailHandler(
-            <>
-                <button type="button" onClick={ () => toggleSelectedTable(Math.ceil(Math.random() * 10)) }>add/remove random selecte tables</button>
-                <menu>
-                    <button type="button" onClick={ () => changeTestConfig({ testStyle: "MULTIPLE_CHOICES" }) }>multiple choices questions</button>
-                    <button type="button" onClick={ () => changeTestConfig({ testStyle: "DIRECT_INPUT" }) }>direct input questions</button>
-                </menu>
-                <menu>
-                    <button type="button" onClick={ () => changeTestConfig({ testMode: "NO_TIME_LIMIT" }) }>no time</button>
-                    <button type="button" onClick={ () => changeTestConfig({ testMode: "SOFT_TIME_LIMIT" }) }>time limit soft</button>
-                    <button type="button" onClick={ () => changeTestConfig({ testMode: "HARD_TIME_LIMIT" }) }>time limit hard</button>
-                </menu>
-            </>
-        );
-    }, [displayDetailHandler, changeTestConfig, toggleSelectedTable]);
+        displayDetailHandler(<QuestionnaireConfigurator onCancelConfigurator={ () => displayDetailHandler(null) } />);
+    }, [displayDetailHandler]);
 
     // ensure details removal when unmounting
     useEffect(() => {
@@ -59,14 +45,14 @@ const TestView:FC<TestViewProps> = ({ displayDetailHandler }) => {
                 triggerTestSetupHandler={ triggerTestConfigSetupHandler }
             >
                 { (testState === "PRE_TEST") 
-                    ?   [
-                            <button type="button" className={ styles['tv-testBtn'] } onClick={ startTestHandler }>start</button>,
+                    ?   <>
+                            <button type="button" className={ styles['tv-testBtn'] } onClick={ startTestHandler }>Démarrer</button>
                             <button type="button" className={ [styles['tv-testBtn'], styles['tv-testBtn--secondary']].join(' ') } onClick={ triggerTestConfigSetupHandler }>configure</button>
-                        ] 
+                        </> 
                     : (testState === "RUN_TEST")
-                    ?   <button type="button" className={ styles['tv-testBtn'] } onClick={ endTestHandler }>end</button>
+                    ?   <button type="button" className={ [styles['tv-testBtn'], styles['tv-testBtn--secondary']].join(' ') } onClick={ endTestHandler }>Abandonner</button>
                     : (testState === "TEST_RESULTS")
-                    ?   <button type="button" className={ styles['tv-testBtn'] } onClick={ resetTestHandler }>reset</button>
+                    ?   <button type="button" className={ styles['tv-testBtn'] } onClick={ resetTestHandler }>Retour</button>
                     : null 
                 }
             </QuestionnaireHeader>
--- packages/blog/utils/Enum2LabelConverters.ts
+++ packages/blog/utils/Enum2LabelConverters.ts
@@ -1,8 +1,8 @@
 export function testConfigLabels(enumValue:string):string {
     switch(enumValue) {
         case "NO_TIME_LIMIT": return "Aucune limite de temps";
-        case "SOFT_TIME_LIMIT": return "Indicateur de temps";
-        case "HARD_TIME_LIMIT": return "Limite de temps";
+        case "SOFT_TIME_LIMIT": return "Limite de temps";
+        case "HARD_TIME_LIMIT": return "Limite de temps par questions";
         case "MULTIPLE_CHOICES": return "Questions à choix multiples";
         case "DIRECT_INPUT": return "Questions à saisie du résultat";
         default:
