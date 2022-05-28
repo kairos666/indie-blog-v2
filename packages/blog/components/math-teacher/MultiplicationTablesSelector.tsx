34b3d8296d3de59d08ecb69a777a8a4c54257b16
David Maggi <musashi.miyamoto81@gmail.com>
2022-05-28 09:10:31 +0200

    accessibility on MultiplicationTablesSelector

--- packages/blog/components/math-teacher/MultiplicationTablesSelector.tsx
+++ packages/blog/components/math-teacher/MultiplicationTablesSelector.tsx
@@ -8,16 +8,32 @@ type MultiplicationTablesSelectorProps = {
 };
 
 const MultiplicationTablesSelector:FC<MultiplicationTablesSelectorProps> = ({ currentlySelected, availableTables, changeHandler }) => {
+    const instanceID:number = 12; // TODO replace with useId (react 18)
+
     return (
-        <ol className={ styles['mts-MenuList'] }>
-            { availableTables.map(entry => {
-                return (
-                    <li className={ styles['mts-MenuListItem'] } key={ entry }>
-                        <button type="button" className={ styles['mts-SelectorBtn'] } aria-pressed={ currentlySelected.includes(entry) } onClick={ () => changeHandler(entry) } >{ entry }</button>
-                    </li>
-                );
-            })}
-        </ol>
+        <>
+            <span id={ `table-selector-${ instanceID }` } className={ styles['sr-only'] }>Sélection des tables de multiplication</span>
+            <ol role="radiogroup" aria-labelledby={ `table-selector-${ instanceID }` } className={ styles['mts-MenuList'] }>
+                { availableTables.map(entry => {
+                    const isOn:boolean = currentlySelected.includes(entry);
+
+                    return (
+                        <li className={ styles['mts-MenuListItem'] } key={ entry }>
+                            <button 
+                                type="button" 
+                                role="radio" 
+                                className={ styles['mts-SelectorBtn'] }
+                                aria-checked={ isOn } 
+                                onClick={ () => changeHandler(entry) }
+                                aria-labelledby={ `table-selector-${ instanceID }-entry-${ entry }` }
+                            >
+                                <span id={ `table-selector-${ instanceID }-entry-${ entry }` }>{ entry }</span>
+                            </button>
+                        </li>
+                    );
+                })}
+            </ol>
+        </>
     )
 }
 
